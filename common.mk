dab5b21e99f850b4c9cf72071e9846e842a3251b
Codecity001 <sanganiharish263@gmail.com>
2025-09-23 14:45:00 +0200

    sm8150-common: Properly label dummy oplus performance HAL service
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    vendor-oplus-hardware-performance-V1-service
+    vendor.oplus.hardware.performance-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
